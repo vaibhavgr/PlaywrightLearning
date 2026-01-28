c3065471341626254e889f26fdf1c3aede0938da
Vaibhav Grover <grovervaibhav17@gmail.com>
2026-01-28 21:07:12 +0530

    tillLoginDone

--- tests/basic.spec.js
+++ tests/basic.spec.js
@@ -1,12 +1,11 @@
 //{} around test beacuse playwright/test exports an object, and we wan to extract only 
 // test properties from that object. Object Destructing. 
-const {test} = require('@playwright/test')
+const {test, expect} = require('@playwright/test')
 
 test('First PW Program' , async({browser}) =>
 {
     const context = await browser.newContext();
     const page = await context.newPage();
-
      //open page 
      await page.goto("https://google.com/");
 });
@@ -14,6 +13,21 @@ test('First PW Program' , async({browser}) =>
 test.only('Second PW Program' , async({browser,page}) =>
 {
      //open page 
-     await page.goto("https://web.whatsapp.com/");
+     await page.goto("https://demowebshop.tricentis.com/");
+     console.log(await page.title())
+     
+     //asserting title of the page
+     await expect(page).toHaveTitle(await page.title());
+     
+     //login locator
+     const loginheaderBtn = page.locator('.ico-login')
+     await expect(loginheaderBtn).toBeVisible();
+     await loginheaderBtn.click();
+
+     //input text 
+     const emailField = await page.locator('#Email').fill('obsqura24@gmail.com');
+     const passwordField = await page.locator('#Password').fill('mypassword');
+     const loginButton = await page.locator("[value='Log in']").click();
+     
      await page.pause();
 });
