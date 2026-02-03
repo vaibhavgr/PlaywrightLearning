ee048300de5a902a4c3b8128bc762fd6ece8877d
Vaibhav Grover <grovervaibhav17@gmail.com>
2026-02-03 23:14:40 +0530

    hoverSecond

--- tests/basicWaitFor.spec.js
+++ tests/basicWaitFor.spec.js
@@ -2,7 +2,9 @@ const {test,expect} = require('@playwright/test');
 
 test.only('auto wait concepts',async({page})=>
 {
-    await page.goto('https://www.noon.com/uae-en/');
-    page.locator().hover();
+    await page.goto("https://www.noon.com/uae-en/");
+    const kidsFashio =  page.locator("[data-qa=\"btn_main_menu_Kids' Fashion\"]").hover();
+    
+
     await page.pause();
 })
