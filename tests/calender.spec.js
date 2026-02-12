77fe060e0e44df38e56747c327f698a421c09943
Vaibhav Grover <grovervaibhav17@gmail.com>
2026-02-12 08:38:34 +0530

    calender

--- tests/calender.spec.js
+++ tests/calender.spec.js
@@ -1,10 +1,38 @@
 const { test, expect } = require('@playwright/test');
 
+test.only('Easy Universal Date Picker', async ({ page }) => {
 
-test('Calender', async ({page }) => {
-await page.goto("https://selenium.qabible.in/date-picker.php");
+  await page.goto("https://selenium.qabible.in/date-picker.php");
 
+  const targetMonth = "April";
+  const targetYear = "2022";
+  const targetDay = "8";
 
+  await page.locator("[data-provide='datepicker']").click();
 
+  // Target date object
+  const targetDateObj = new Date(`${targetMonth} 1, ${targetYear}`);
+
+  while (true) {
+
+    const uiText = (await page.locator('.datepicker-switch').first().textContent()).trim();
+
+    console.log("UI:", uiText);
+
+    if (uiText === `${targetMonth} ${targetYear}`) break;
+
+    const uiDateObj = new Date(uiText);
+
+    if (uiDateObj > targetDateObj) {
+      await page.locator('.prev').first().click();
+
+    } else {
+      await page.locator('.next').first().click();
+    }
+  }
+
+  // Click day
+  await page.locator(`//td[normalize-space()='${targetDay}']`).click();
+  await page.pause();
 
 });
