ea6f488cedf0ca644dadc2fe3971e379ff403192
Vaibhav Grover <grovervaibhav17@gmail.com>
2026-02-18 10:52:13 +0530

    DeleteAPICart

--- tests/apiIntegration.spec.js
+++ tests/apiIntegration.spec.js
@@ -1,49 +1,69 @@
-const {test , expect , request} = require('@playwright/test');
+const { test, expect, request } = require('@playwright/test');
 
 
 const loginURL = "https://api.demoblaze.com/login";
 const addtoCartURL = "https://api.demoblaze.com/addtocart";
 const payload = {
-    username : "vaibhav@yopmail.com",
-    password : "cXdlcnR5MTIz",
+    username: "vaibhav@yopmail.com",
+    password: "cXdlcnR5MTIz",
 }
-
-
-test.beforeAll(async()=>
-{
-const apiRequest = await request.newContext();
-const response = await apiRequest.post(loginURL, {
-    data: payload
-})
-//fetch response Auth token 
-console.log(await response.text())
-const addToCartResponse = await apiRequest.post(addtoCartURL , {
-    data : {
-    id : "230817b8-5d90-9ba5-c9c8-fd5c18b34338",
-    cookie : "dmFpYmhhdkB5b3BtYWlsLmNvbTE3NzE5MTQ=",
-    flag : "true",
-    prod_id : "1",
+const prodMap = {
+    1: "d55b727e-363e-94fc-1a53-cf1390b205a5",
+    2: "78bef5ff-f433-1726-7c6b-7e9c04b24a60",
+    4: "170ba846-8ac0-50e0-07e1-0707aee86a79"
 }
-})
+const deleteItemCartURL = "https://api.demoblaze.com/deleteitem";
+test.beforeAll(async () => {
+    const apiRequest = await request.newContext();
+    const response = await apiRequest.post(loginURL, {
+        data: payload
+    })
+    //fetch response Auth token 
+    const authToken = (await response.text()).split(":")[1].trim().replace('"', "");
+    for (const key in prodMap) {
+        const addToCartResponse = await apiRequest.post(addtoCartURL, {
+            data: await getCartpayload(prodMap[key], key, authToken)
+        }
+        )
+        expect(addToCartResponse.ok()).toBeTruthy();
+    };
 
-})
-
-
-
-test.only('Api Integration', async({page})=>{
-await page.goto('https://www.demoblaze.com');
-await login(page);
-await page.pause();
-
-
-async function login(page){
-await page.getByRole('link', { name : 'Log in'}).click();
-await page.locator('#loginusername').fill('vaibhav@yopmail.com');
-await page.locator('#loginpassword').fill('qwerty123');
-await page.getByRole('button', {name:'Log in'}).click();
-await page.getByRole('link', { name: 'Cart' }).click();
-
-}
+    const cartDelResponse = await apiRequest.post(deleteItemCartURL , 
+        {
+            data : {
+                id : "78bef5ff-f433-1726-7c6b-7e9c04b24a60",
+            }
+        }
+    )
+    expect(cartDelResponse.ok()).toBeTruthy();
 
 });
+
+
+
+test.only('Api Integration', async ({ page }) => {
+    await page.goto('https://www.demoblaze.com');
+    await login(page);
+});
+
+
+    async function login(page) {
+        await page.getByRole('link', { name: 'Log in' }).click();
+        await page.locator('#loginusername').fill('vaibhav@yopmail.com');
+        await page.locator('#loginpassword').fill('qwerty123');
+        await page.getByRole('button', { name: 'Log in' }).click();
+        await page.getByRole('link', { name: 'Cart' }).click();
+
+    }
+
+
+    async function getCartpayload(id, prod_id, cookie) {
+        return {
+            id: id,
+            cookie: cookie,
+            flag: 'true',
+            prod_id: prod_id
+        }
+    }
+
 
