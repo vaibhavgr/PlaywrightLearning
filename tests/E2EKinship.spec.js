fec0f9b29da2d0a6c5aedb17fd5deb123f2c38cf
Vaibhav Grover <grovervaibhav17@gmail.com>
2026-02-25 15:51:16 +0530

    APIUtils

--- tests/E2EKinship.spec.js
+++ tests/E2EKinship.spec.js
@@ -2,24 +2,28 @@ const {test , request} = require('@playwright/test');
 const{APIutils} = require('./Utils/APIutils.js');
 
 
-
+let token
 //API Part- test.beforeALL
 test.beforeAll(async()=>{
     const apiContext = await request.newContext();
     const apiUtilsPage = new APIutils(apiContext);
-    const token = await apiUtilsPage.authorizeToken()
+    token = await apiUtilsPage.authorizeToken()
+    const addpet = await apiUtilsPage.addpet();
     console.log(token);
-
-
-    
+    console.log(addpet)
 })
-
-
-
-
 
 //FRONETENT UI -----------------------------------------------------------------------------------
 test.only('E2E Kinship Pet Test', async ({page }) => {
 await page.goto("https://www.stg.kinship.com/uk");
+await page.addInitScript(value=>{
+  window.localStorage.setItem('access_token',value);  
+}, token)
+//addpet
+await page.getByRole('button',{name: 'Reject All'}).click();
+//await page.getByRole('button',{name: 'Maybe Later'}).click();
+await page.locator("[opti-default-header-user-account-button='user-account-button']").click();
+await page.getByText('Add a pet').click();
+await page.pause();
 
 });
--- tests/Utils/APIutils.js
+++ tests/Utils/APIutils.js
@@ -1,5 +1,6 @@
 
 const loginURL = "https://kinship-api-staging.kinship.engineering/auth/v2-sign-in";
+const addPetURL = "https://kinship-api-staging.kinship.engineering/my-pet-profile";
 
 class APIutils {
 
@@ -7,23 +8,31 @@ class APIutils {
         this.apiContext = apiContext;
     }
 
-
-
     loginPayload() {
         return { email: "22jan@test.com", password: "qwerty1234" };
     }
 
-    async postcall(URL, payload) {
-        return await this.apiContext.post(URL, {
-            data: payload,
-            headers: this.getKinshipHeaders()
-        })
+   async postcall(URL, payload, customHeaders = {}) {
+    return await this.apiContext.post(URL, {
+        data: payload,
+        headers: {
+            ...this.getKinshipHeaders(),
+            ...customHeaders
+        }
+    })
+}
+    async addpetToken(){
+    const token = await this.authorizeToken();
+    return {
+        "Authorization": `Bearer ${token}`,  
     }
+}
+    
+    
 
-    async authorizeToken()
-    {
-        const response  = await this.postcall(loginURL, this.loginPayload());
-        const jsonResponse =await response.json()
+    async authorizeToken() {
+        const response = await this.postcall(loginURL, this.loginPayload());
+        const jsonResponse = await response.json()
         return jsonResponse['tokens']['accessToken'];
     }
 
@@ -31,8 +40,28 @@ class APIutils {
         return {
             "accept-language": "en-GB"
         }
+    }
 
+   async addpet() {
+    
+        await this.postcall(addPetURL,this.addpetPayload(),await this.addpetToken() )
+    }
+    
 
+    addpetPayload() {
+        return {
+            "name": "Jimmyys",
+            "animal_type": "DOG",
+            "breed": "Africanis Dog",
+            "birth_date": "2021-09-01",
+            "gender": "MALE",
+            "spayed_or_neutered": "true",
+            "weight": "10",
+            "acquisition_date": "2024-04-03",
+            "acquisition_date_precision": "DAY",
+            "wisdom_breed_id_1": "07f2d080-9b49-4e36-94e0-639303c5ae1b",
+           
+        }
     }
 
 
