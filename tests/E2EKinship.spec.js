539fcc56868aac04db2bf5b35b3f0f090fa56d08
Vaibhav Grover <grovervaibhav17@gmail.com>
2026-02-27 20:22:47 +0530

    Pet Removal

--- tests/E2EKinship.spec.js
+++ tests/E2EKinship.spec.js
@@ -3,6 +3,7 @@ const{APIutils} = require('./Utils/APIutils.js');
 
 
 let tokens
+let addpet
 //API Part- test.beforeALL
 test.beforeAll(async()=>{
     const apiContext = await request.newContext({
@@ -10,13 +11,14 @@ test.beforeAll(async()=>{
 });
     const apiUtilsPage = new APIutils(apiContext);
     tokens = await apiUtilsPage.authorizeToken()
-    const addpet = await apiUtilsPage.addpet(tokens);
-    //console.log(token);
-    console.log(addpet)
+    addpet = await apiUtilsPage.addpet(tokens);
+    console.log(tokens , addpet);
+    
+    
 })
 
 //FRONETENT UI -----------------------------------------------------------------------------------
-test.only('E2E Kinship Pet Test', async ({page }) => {
+test('E2E Kinship Add Pet', async ({page }) => {
 await page.goto("https://www.stg.kinship.com/uk");
 await page.addInitScript(value=>{
   window.localStorage.setItem('access_token',value);  
@@ -26,6 +28,26 @@ await page.getByRole('button',{name: 'Reject All'}).click();
 //await page.getByRole('button',{name: 'Maybe Later'}).click();
 await page.locator("[opti-default-header-user-account-button='user-account-button']").click();
 await page.getByText('Add a pet').click();
+await page.pause();
+
+});
+
+
+test.only('E2E Kinship Pet Removal', async ({page})=>{
+await page.goto("https://www.stg.kinship.com/uk");
+await page.addInitScript(value=>{
+  window.localStorage.setItem('access_token',value);  
+}, tokens)
+//addpet
+await page.getByRole('button',{name: 'Reject All'}).click();
+//await page.getByRole('button',{name: 'Maybe Later'}).click();
+await page.locator("[opti-default-header-user-account-button='user-account-button']").click();
+await new Promise(res=>setTimeout(res, 6000));
+const postPetName = page.getByText(`${addpet}`);
+await postPetName.waitFor();
+const petCard = page.locator(`//section//*[contains(text(),"${addpet}")]/../..`);
+await petCard.getByText('Remove').click();
+await page.getByRole('button', {name : 'Yes, Remove This Pet'}).click();
 await page.pause();
 
 });
